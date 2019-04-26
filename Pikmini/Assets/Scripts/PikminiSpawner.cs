e5c03f3850708ff97a8bd03fe8f0fb368277afe5
cmnlee <cmnlee@ucdavis.edu>
2019-04-26 13:41:36 -0700

    Basic Spawn functionality added.

--- Pikmini/Assets/Scripts/PikminiSpawner.cs
+++ Pikmini/Assets/Scripts/PikminiSpawner.cs
@@ -1,7 +1,27 @@
+using UnityEngine;
+
 namespace Pikmini
 {
-    public class PikminiSpawner
+    public class PikminiSpawner : MonoBehaviour
     {
-        
+        [SerializeField]
+        private GameObject MiniPrefab;
+        [SerializeField] 
+        private GameObject SpawnPoint;
+
+        private void Start()
+        {
+            
+        }
+
+        // todo: maybe use pubsub instead of update().
+        private void Update()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                MiniPrefab = Instantiate(this.MiniPrefab, this.SpawnPoint.transform.position, Quaternion.identity);
+            }
+        }
+
     }
 }
